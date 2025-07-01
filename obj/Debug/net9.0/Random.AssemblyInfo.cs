0a55a5c37a30f8f2326642ce3a234382c741507b
Cameron Duckett <114952118+Ducky-007@users.noreply.github.com>
2025-07-01 11:20:27 -0700

    "adds files to project"

--- obj/Debug/net9.0/Random.AssemblyInfo.cs
+++ obj/Debug/net9.0/Random.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Random")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a202129b3e564f848fbd1d93db8f5033e773c40f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Random")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Random")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
